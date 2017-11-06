a380f442c4d74d30bb1936b6a4fecb4d30a640ab
lrygit <357478165@qq.com>
2017-11-06 23:25:54 +0800

    11.6

--- Assets/script/index.cs
+++ Assets/script/index.cs
@@ -7,13 +7,15 @@ public class index : MonoBehaviour {
     public GameObject old_panel;
     public GameObject confirm_panel;
     public bool confirm_flag;
-    public bool isOperation;
 
     // Use this for initialization
     void Start () {
         UIEventListener.Get(GameObject.Find("UI Root").transform.Find("option_new").gameObject).onHover +=on_hover;
         UIEventListener.Get(GameObject.Find("UI Root").transform.Find("option_old").gameObject).onHover += on_hover;
         UIEventListener.Get(GameObject.Find("UI Root").transform.Find("option_quit").gameObject).onHover += on_hover;
+        UIEventListener.Get(confirm_panel.transform.Find("true").gameObject).onClick += operation_confirm;
+        UIEventListener.Get(confirm_panel.transform.Find("false").gameObject).onClick += operation_confirm;
+        UIEventListener.Get(GameObject.Find("UI Root").transform.Find("option_old").gameObject).onClick += go_old;
     }
 	
 	// Update is called once per frame
@@ -28,13 +30,19 @@ public class index : MonoBehaviour {
             g1.transform.Find("option_hover").gameObject.SetActive(false);
     }
 
-    public void go_old() {
+    public void go_old(GameObject g1) {
         old_panel.SetActive(true);
     }
 
     public void operation_confirm(GameObject g1) {
         confirm_flag = bool.Parse (g1.name);
-        isOperation = true;
+        if (confirm_flag)
+        {
+            Application.Quit();
+        }
+        else {
+            confirm_panel.SetActive(false);
+        }
     }
 
      
